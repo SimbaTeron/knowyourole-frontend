9bfc766dec5e03084f5e6bc1da9501961432364d
Alfred <alfred@knowyourole.com>
2026-03-24 13:13:32 -0700

    FIX: Render auth page content properly

--- KnowYouRole/client/src/App.tsx
+++ KnowYouRole/client/src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { Switch, Route, useLocation, Redirect } from "wouter";
-import { Auth0Provider } from "@auth0/auth0-react";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -65,19 +64,13 @@ function Router() {
 
 function App() {
   return (
-    <Auth0Provider
-      domain="dev-f0lrnyg4uigdvae1.us.auth0.com"
-      clientId="edp3GnoatCBItXXQiu6jsxjL0Tc3CTIM"
-      cacheLocation="localstorage"
-    >
-      <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
-          <ScrollToTop />
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </QueryClientProvider>
-    </Auth0Provider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <ScrollToTop />
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 }
 
